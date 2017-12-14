cb065dee9ed10df47dc8c3a7d7f914c36743b1ab
Ramon Henrique <rhpr@cin.ufpe.br>
2017-12-14 16:13:07 -0300

    Create dockerfile

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mongod --fork --logpath /var/log/mongodb.log &
+/etc/knot/start.sh knot-fog 3&> /dev/null
+/etc/knot/start.sh knot-web 3&> /dev/null
+/etc/knot/start.sh knotd 3&> /dev/null
+/etc/knot/start.sh nrfd 3&> /dev/null
+/etc/knot/start.sh lorad 3&> /dev/null
+/bin/bash
