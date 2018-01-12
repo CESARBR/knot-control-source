66a29bf5cf06d0037c4ae59f58ea92438a8b8c9c
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-01-12 18:00:12 -0300

    Add `get cloud` command to CLI

--- src/cli/CLISettingsProgram.js
+++ src/cli/CLISettingsProgram.js
@@ -1,25 +1,47 @@
 import yargs from 'yargs';
 
+/* eslint-disable no-unused-expressions, no-console */
+async function execute(handler, args) {
+  try {
+    await handler(args);
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+function createHandler(handlerImpl) {
+  return async (args) => {
+    await execute(handlerImpl, args);
+  };
+}
+
 class CLISettingsProgram {
   constructor(settingsApi) {
     this.settingsApi = settingsApi;
   }
 
-  /* eslint-disable no-unused-expressions, no-console */
   async run() {
     yargs
       .command('get <property>', 'Gets the property value', _yargs => _yargs
-        .command('ready', 'Gets whether this gateway is ready', {}, async () => {
-          try {
-            const isReady = await this.settingsApi.isReady();
-            console.log(isReady ? 'Ready' : 'Not ready');
-          } catch (err) {
-            console.error(err.message);
-          }
-        }))
+        .command('ready', 'Gets whether this gateway is ready', {}, createHandler(this.isReady.bind(this)))
+        .command('cloud', 'Gets the cloud configuration', {}, createHandler(this.getCloud.bind(this))))
       .demandCommand()
       .strict()
       .argv;
+  }
+
+  async isReady() {
+    const isReady = await this.settingsApi.isReady();
+    console.log(isReady ? 'Ready' : 'Not ready');
+  }
+
+  async getCloud() {
+    const configuration = await this.settingsApi.getCloud();
+    if (configuration) {
+      console.log(`${configuration.hostname}:${configuration.port}`);
+    } else {
+      console.log('Not configured');
+    }
   }
 }
 /* eslint-enable */
--- test/cli/CLISettingsAPI.spec.js
+++ test/cli/CLISettingsAPI.spec.js
@@ -2,11 +2,13 @@ import tape from 'tape';
 import around from 'tape-around';
 import sinon from 'sinon';
 import CLISettingsAPI from 'cli/CLISettingsAPI';
+import Address from 'entities/Address';
 
 const test = around(tape)
   .before((t) => {
     const settingsService = {
       isReady: sinon.stub().resolves(true),
+      getCloud: sinon.stub().resolves(new Address('localhost', 3000)),
     };
     const cliSettingsAPI = new CLISettingsAPI(settingsService);
     t.next(cliSettingsAPI);
@@ -16,5 +18,30 @@ test('isReady() calls SettingsService.isReady()', async (t, cliSettingsAPI) => {
   await cliSettingsAPI.isReady();
 
   t.true(cliSettingsAPI.settingsService.isReady.called);
+  t.end();
+});
+
+test('isReady() returns result returned by SettingsService', async (t, cliSettingsAPI) => {
+  const expectedResult = true;
+
+  const actualResult = await cliSettingsAPI.isReady();
+
+  t.equal(actualResult, expectedResult);
+  t.end();
+});
+
+test('getCloud() calls SettingsService.getCloud()', async (t, cliSettingsAPI) => {
+  await cliSettingsAPI.getCloud();
+
+  t.true(cliSettingsAPI.settingsService.getCloud.called);
+  t.end();
+});
+
+test('getCloud() returns Address returned by SettingsService', async (t, cliSettingsAPI) => {
+  const expectedAddress = new Address('localhost', 3000);
+
+  const actualAddress = await cliSettingsAPI.getCloud();
+
+  t.deepEquals(actualAddress, expectedAddress);
   t.end();
 });
