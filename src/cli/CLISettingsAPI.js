bd59030a6bfd64042a1f9846963fa43276ac00a2
João Neto <joao.alexandre@cesar.org.br>
2018-01-26 08:37:20 -0300

    Add `set user` command to CLI

--- src/cli/CLISettingsAPI.js
+++ src/cli/CLISettingsAPI.js
@@ -1,4 +1,5 @@
 import { ConfigureCloudRequest } from 'services/ConfigureCloudRequest';
+import { SetUserRequest } from 'services/SetUserRequest';
 
 class CLISettingsAPI {
   constructor(settingsService) {
@@ -16,6 +17,11 @@ class CLISettingsAPI {
   async configureCloud(hostname, port) {
     const request = new ConfigureCloudRequest(hostname, port);
     await this.settingsService.configureCloud(request);
+  }
+
+  async setUser(uuid, token) {
+    const request = new SetUserRequest(uuid, token);
+    await this.settingsService.setUser(request);
   }
 }
 
